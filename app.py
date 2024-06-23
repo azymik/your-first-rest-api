c481adf68c7d1c27889e530c47d93eb26b47ff90
azymik <78275041+azymik@users.noreply.github.com>
2024-06-23 16:02:08 +0000

    add create_item

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ stores = [
 def get_stores():
     return {'stores': stores}
 
-@app.post('/store')
+@app.post('/store') # http://localhost:6500/store name='my store' 
 def create_store():
     request_data = request.get_json()
     new_store = {
@@ -28,3 +28,16 @@ def create_store():
     }
     stores.append(new_store)
     return new_store, 201
+
+@app.post('/store/<string:name>/item') # http http://localhost:5000/store/My\ Store/item name='Table' price:=17.99
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price'],
+            }
+            store['items'].append(new_item)
+            return new_item, 201
+    return {'message': 'Store not found'}, 404
