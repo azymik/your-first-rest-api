3629bdafe7604bd678a7d1cba35688ee1a91d1f8
azymik <78275041+azymik@users.noreply.github.com>
2024-06-24 16:00:11 +0000

    fixed start container

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-code .
 docker buildx build $(cat .devpass.env | sed 's@^@--build-arg @g' | paste -s -d " " /dev/stdin) --progress=plain -t azymik/flask--udemy-course-rest-api-with-flask-and-python-in-2024--your-first-rest-api .
 docker container run -it --rm -v ./:/app -p 65000:5000 azymik/flask--udemy-course-rest-api-with-flask-and-python-in-2024--your-first-rest-api
